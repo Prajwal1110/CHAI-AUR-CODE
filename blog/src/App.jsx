6ca6f564558481c039180e17a1fea21d7facdc6d
Prajwal1110 <01fe21bcs159@kletech.ac.in>
2024-01-14 00:19:45 +0530

    FIrst commit

---
+++ blog/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+  // console.log(process.env.REACT_APP_APPWRITE_URL);
+  return (
+ 
+    <>
+     <h1>A blog with app appwrite
+      npo
+     </h1>
+    </>
+  )
+}
+
+export default App
